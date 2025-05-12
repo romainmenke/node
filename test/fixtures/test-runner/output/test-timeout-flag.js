0c3e8dc2d4399f654a164730802497e6a36f3a35
Romain Menke <romainmenke@gmail.com>
2025-05-12 12:17:54 +0200

    test: await sub tests in test-timeout-flag.js

--- test/fixtures/test-runner/output/test-timeout-flag.js
+++ test/fixtures/test-runner/output/test-timeout-flag.js
@@ -3,17 +3,17 @@
 const { describe, test } = require('node:test');
 const { setTimeout } = require('node:timers/promises');
 
-describe('--test-timeout is set to 20ms', () => {
-  test('should timeout after 20ms', async () => {
+describe('--test-timeout is set to 20ms', async () => {
+  await test('should timeout after 20ms', async () => {
     await setTimeout(200000, undefined, { ref: false });
   });
-  test('should timeout after 5ms', { timeout: 5 }, async () => {
+  await test('should timeout after 5ms', { timeout: 5 }, async () => {
     await setTimeout(200000, undefined, { ref: false });
   });
 
-  test('should not timeout', { timeout: 50000 }, async () => {
+  await test('should not timeout', { timeout: 50000 }, async () => {
     await setTimeout(1);
   });
 
-  test('should pass', async () => {});
+  await test('should pass', async () => {});
 });
