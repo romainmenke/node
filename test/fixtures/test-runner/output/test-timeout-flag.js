aecefe4ddf6d9d60131d252633f6d77aa4bc62e8
Romain Menke <romainmenke@gmail.com>
2025-05-12 12:24:10 +0200

    test: await sub tests in test-timeout-flag.js

--- test/fixtures/test-runner/output/test-timeout-flag.js
+++ test/fixtures/test-runner/output/test-timeout-flag.js
@@ -7,6 +7,7 @@ describe('--test-timeout is set to 20ms', async () => {
   await test('should timeout after 20ms', async () => {
     await setTimeout(200000, undefined, { ref: false });
   });
+
   await test('should timeout after 5ms', { timeout: 5 }, async () => {
     await setTimeout(200000, undefined, { ref: false });
   });
